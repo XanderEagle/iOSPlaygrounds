7b0c9270db88a73edbd4581c9114212cf48cb106
Xander <alexander.eagle@ssfs.org>
2020-02-13 15:13:12 -0500

    finished variables

---
+++ trafficLights/trafficLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLights
+//
+//  Created by Xander on 2/13/20.
+//  Copyright © 2020 Xander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
